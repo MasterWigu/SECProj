5f4d8513dca9201de4494cb57be57e83c550b2bc
MasterWigu <moreira.oliveira@live.com.pt>
2020-03-16 17:22:10 +0000

    asljdbaskld

---
+++ commonClasses/src/main/java/commonClasses/TTTService.java
@@ -0,0 +1,16 @@
+package ttt;
+
+import java.rmi.*;
+import java.security.PublicKey;
+
+/** Tic Tac Toe game remote interface. */
+public interface TTTService extends Remote {
+    String register(PublicKey key, String name) throws RemoteException;
+
+    String post(PublicKey key, String message, Annoucement player) throws RemoteException;
+
+    int checkWinner() throws RemoteException;
+
+    boolean jogaCantoAleatorio() throws RemoteException;
+
+}
